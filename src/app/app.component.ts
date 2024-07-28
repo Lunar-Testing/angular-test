7493c187d83f110a8283908530507788e38be8dd
Felix Wurzinger <wurzinger@lunartainment.com>
2024-07-28 23:39:01 +0200

    Added test component and angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponentComponent } from './test-component/test-component.component';
+// Button needs to be imported here!
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // Imported button needs to also be in here! example: MatButtonModule
+  imports: [RouterOutlet, TestComponentComponent, MatButtonModule, MatButtonToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
